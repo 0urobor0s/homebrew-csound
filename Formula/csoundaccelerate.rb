fe39da6afb053bc8b780f0680b02d43c66af6996
Calvin Bottoms <calvin.bottoms@gmail.com>
2016-08-27 15:05:15 -0500

    Updated another sha1 to sha256

--- Formula/csoundaccelerate.rb
+++ Formula/csoundaccelerate.rb
@@ -5,7 +5,7 @@ class Csoundaccelerate < Formula
     url 'https://github.com/eddyc/CsoundAccelerate/archive/v0.001.tar.gz'
     head 'https://github.com/eddyc/CsoundAccelerate.git'
     version '0.001'
-    sha1 '68855a6c1e6f96ca4473c6f098c02acd8bc7e545'
+    sha256 '3b14f81cc13f218b73919c504284012e49e5f5235d7592dc0def6c3fbbcdccc1'
     
     depends_on 'hdf5'
     depends_on 'Csound'
